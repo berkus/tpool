7eaa769b742d86010a2be2cc44919823dffede13
Berkus <berkus@gmail.com>
2013-04-17 17:56:56 +0300

    Fix std::result_of<>

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 //
 //  main.cpp
-//  righterpool
+//  richterpool
 //
-//  Created by zz on 04.17.13.
+//  Created by zz on 2013.04.17.
 //  Copyright (c) 2013 zz. All rights reserved.
 //
 #include <thread>
@@ -13,125 +13,6 @@
 #include <future>
 #include <utility>
 #include <type_traits>
-
-///=====================
-//
-template <typename C>
-struct has_call_operator
-{
-        typedef char (&yes)[2];
-        typedef char (&no)[1];
-        
-        template <typename R>
-        static yes check(decltype(&R::operator()));
-        
-        template <typename>
-        static no check(...);
-        
-        static const bool value = sizeof(check<C>(0)) == sizeof(yes);
-};
- 
-template <typename, bool Use>
-struct result_of_free
-// dummy type, std::conditional doesn't do lazy checking;
-// should not be ever used
-{
-        typedef void type;
-};
- 
-template <typename R, typename... A>
-struct result_of_free<R (*)(A...), true>
-{
-        typedef R type;
-};
- 
-template <typename R, typename... A>
-struct result_of_free<R (*)(A......), true>
-{
-        typedef R type;
-};
- 
-template <typename, bool Use>
-struct result_of_memfn_impl
-{ };
- 
-template <typename C, typename R, typename... A>
-struct result_of_memfn_impl<R (C::*)(A...), true>
-{
-        typedef R type;
-};
- 
-template <typename C, typename R, typename... A>
-struct result_of_memfn_impl<R (C::*)(A...) const, true>
-{
-        typedef R type;
-};
- 
-template <typename C, typename R, typename... A>
-struct result_of_memfn_impl<R (C::*)(A...) volatile, true>
-{
-        typedef R type;
-};
- 
-template <typename C, typename R, typename... A>
-struct result_of_memfn_impl<R (C::*)(A...) const volatile, true>
-{
-        typedef R type;
-};
- 
-template <typename C, typename R, typename... A>
-struct result_of_memfn_impl<R (C::*)(A......), true>
-{
-        typedef R type;
-};
- 
-template <typename C, typename R, typename... A>
-struct result_of_memfn_impl<R (C::*)(A......) const, true>
-{
-        typedef R type;
-};
- 
-template <typename C, typename R, typename... A>
-struct result_of_memfn_impl<R (C::*)(A......) volatile, true>
-{
-        typedef R type;
-};
- 
-template <typename C, typename R, typename... A>
-struct result_of_memfn_impl<R (C::*)(A......) const volatile, true>
-{
-        typedef R type;
-};
- 
-template <typename, bool Use>
-struct result_of_memfn
-// dummy type, std::conditional doesn't do lazy checking;
-// should not be ever used
-{
-        typedef void type;
-};
- 
-template <typename C>
-struct result_of_memfn<C, true>
-{
-        typedef typename result_of_memfn_impl<decltype(&C::operator()), true>::type type;
-};
- 
-template <typename T>
-struct result_of
-// yeah, I know, quick and dirty
-{
-        static const bool has_op = has_call_operator<T>::value;
-        typedef typename
-                std::conditional<
-                        has_op,
-                        typename result_of_memfn<T, has_op>::type,
-                        typename result_of_free<T, !has_op>::type
-                >::type type; 
-};
-//
-//=====================
-
 
 struct ITask {
     virtual void execute() = 0;
@@ -163,14 +44,14 @@ public:
         }
     }
 
-    // http://en.cppreference.com/w/cpp/types/result_of does not work :(
+    // http://en.cppreference.com/w/cpp/types/result_of
 
-    template<typename _Callable>
-    std::future<typename std::result_of<_Callable>::type> execute_async(_Callable func) {
-        std::packaged_task<_Callable> task(func);
+    template<typename Callable>
+    std::future<typename std::result_of<Callable()>::type> execute_async(Callable func) {
+        std::packaged_task<Callable> task(func);
         auto result = task.get_future();
         
-        task_impl<typename std::result_of<_Callable>::type> t(std::move(task));
+        task_impl<typename std::result_of<Callable()>::type> t(std::move(task));
 		
         std::lock_guard<std::mutex> _(lock_);
         tasks_.push(std::move(t));
